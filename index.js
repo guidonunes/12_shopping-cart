c2420bbcbe9a35782c0f3a81b9c0965dfd0695f9
Guilherme Nunes <guilherme.augd@gmail.com>
2024-06-28 09:46:45 -0300

    implement push method

--- index.js
+++ index.js
@@ -112,5 +112,6 @@ class ShoppingCart {
   addItem(id, products) {
     const product = products.find((item) => item.id === id );
     const{name, price} = product;
+    this.items.push(product);
   }
 };
