7a9d0116d8dbb9738ac0ae4901c86eb457d9b1be
Guilherme Nunes <guilherme.augd@gmail.com>
2024-06-30 11:14:14 -0300

    add two p elements and set the value of the second to price

--- index.js
+++ index.js
@@ -119,6 +119,12 @@ class ShoppingCart {
     });
     const currentProductCount = totalCountPerProduct[product.id];
     const currentProductCountSpan = document.getElementById(`product-count-for-id${product.id}`)
-    currentProductCount > 1 ? currentProductCountSpan.textContent = `${currentProductCount}x`: productsContainer.innerHTML += `<div class="product" id="dessert${id}"></div>`;
+    currentProductCount > 1
+      ? currentProductCountSpan.textContent = `${currentProductCount}x`
+      : productsContainer.innerHTML += `
+      <div class="product" id="dessert${id}">
+        <p></p>
+        <p>${price}</p>
+      </div>`;
   }
 };
