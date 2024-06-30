66bef591783f73f935b102d5633b3ae5ddd2a6e2
Guilherme Nunes <guilherme.augd@gmail.com>
2024-06-30 16:38:29 -0300

    add call-back function and event listener

--- index.js
+++ index.js
@@ -133,4 +133,7 @@ class ShoppingCart {
 
 const cart = new ShoppingCart();
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
-[...addToCartBtns].forEach();
+[...addToCartBtns].forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+  })
+});
