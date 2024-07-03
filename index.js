7dcd3c9bd7789e061ad5a788f4233199c5101da2
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-03 20:16:13 -0300

    set textContent of cartTaxes & cartTotal

--- index.js
+++ index.js
@@ -143,7 +143,9 @@ class ShoppingCart {
     const tax = this.calculateTaxes(subTotal);
     this.total = subTotal + tax;
     cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
-  }
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+  };
 };
 
 
